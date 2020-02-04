4c5f49228b5fb5506d2ac89d82c86108dbd14bde
scirocco <zyf199966@126.com>
2020-02-04 23:23:56 +0800

    feat: count entire disp with opencv

--- README.md
+++ README.md
@@ -4,12 +4,50 @@
 
 目前ROS上的OpenCV版本默认为opencv3，非常舒适。
 
+## api中的效果
+
+*   sgbm方式速度极慢，仅计算视差图时fps都只有4左右，但是效果似乎不错；
+*   bm方式速度很快，仅计算视差图的话fps在20上下波动，不过图块连片，目测效果很差。
+
+因此考虑进行改写，由于本项目仅需对yolo框选出的物体进行测距，因此可以减少视差计算范围，加快速度。
+
+目前计划使用sgbm算法。
+
 ## 参考教程
 
 以下均为[Eason.wxd](https://me.csdn.net/App_12062011)的系列博客：
 * [双目测距（一）：图像获取与单目标定](https://blog.csdn.net/App_12062011/article/details/52032563)
 * [双目测距（二）：双目标定与矫正](https://blog.csdn.net/App_12062011/article/details/52032667)
-* [双目测距（三）：立体匹配](https://blog.csdn.net/App_12062011/article/details/52032935)
+* [**双目测距（三）：立体匹配**](https://blog.csdn.net/App_12062011/article/details/52032935)
 * [双目测距（四）：罗德里格斯变换](https://blog.csdn.net/App_12062011/article/details/52033328)
 * [双目测距（五）：匹配算法对比](https://blog.csdn.net/App_12062011/article/details/52034855)
 * [双目测距（六）：三维重建及UI显示](https://blog.csdn.net/App_12062011/article/details/52034906)
+
+### 局部匹配准则
+
+以 **SAD** （Sum of Absolute Differences）为例：
+
+SAD 算法：SAD算法是一种最简单的匹配算法，用公式表示为：
+
+SAD(u,v) = Sum{|Left(u,v) - Right(u,v)|}  选择最小值
+
+此种方法就是以左目图像的源匹配点为中心，定义一个窗口D，其大小为（2m+1） (2n+1)，
+统计其窗口的灰度值的和，然后在右目图像中逐步计算其左右窗口的灰度和的差值，
+最后搜索到的差值最小的区域的中心像素即为匹配点。
+
+### preFilterCap 匹配图像预处理
+
+两种立体匹配算法都要先对输入图像做预处理，OpenCV源码中中调用函数
+`static void prefilterXSobel(const Mat& src, Mat& dst, int preFilterCap)`，
+参数设置中preFilterCap在此函数中用到。
+函数步骤如下，作用主要有两点：对于无纹理区域，能够排除噪声干扰；
+对于边界区域，能够提高边界的区分性，利于后续的匹配代价计算：
+1.  先利用水平Sobel算子求输入图像x方向的微分值Value；
+    *   如果Value<-preFilterCap, 则Value=0;
+    *   如果Value>preFilterCap,则Value=2*preFilterCap;
+    *   如果Value>=-preFilterCap &&Value<=preFilterCap,则Value=Value+preFilterCap
+2.  输出处理后的图像作为下一步计算匹配代价的输入图像。
+
+## TODO
+
+双目图像合并为一张再传给yolo，来达到扩展视野的效果。
--- mynteye_demo.cc
+++ mynteye_demo.cc
@@ -13,8 +13,12 @@
 // limitations under the License.
 #include <stdio.h>
 #include <iostream>
-#include <opencv2/highgui/highgui.hpp>
-
+// #include <opencv2/highgui/highgui.hpp>
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/core/utility.hpp"
 #include "mynteye/api/api.h"
 #include "mynteye/device/device.h"
 
@@ -22,25 +26,38 @@ MYNTEYE_USE_NAMESPACE
 
 int main(int argc, char *argv[])
 {
-    std::cout << ((CV_8UC1) >> CV_CN_SHIFT) + 1 << std::endl;
+    const int width = 640;
+    const int height = 400;
+    const int channel = 1;
 
     std::shared_ptr<mynteye::API> api;
     api = API::Create(argc, argv);
-    // try
-    // {
-    //     static std::shared_ptr<mynteye::Device> BiDevice = mynteye::Device::Create("MYNT-EYE-S2110", device);
-    // }
-    // catch(const std::exception& e)
-    // {
-    //     std::cerr << e.what() << '\n';
-    // }
 
     if (!api)
         return 1;
 
+    // opencv为保证左目的参考点能在右目中找到，会跳过左侧nDisp - 1列像素
+    // The max disparity must be a positive integer divisible by 16
+    // numberOfDisparities，视差窗口，即最大视差值与最小视差值之差
+    const int nDisp = ((width / 8) + 15) & -16; // 80
+    // The block size must be a positive odd number, normally >= 3 and <= 11
+    // SADWindowSize
+    const int SADWin = 5;
+    // preFilterCap，预处理滤波器的截断值，opencv里都设63（？？）
+    const int preFC = 63;
+    // penalty on the disparity change by plus or minus 1 between neighbor pixels
+    const int P1 = 8 * channel * SADWin * SADWin; // 200
+    // penalty on the disparity change by more than 1 between neighbor pixels
+    const int P2 = 4 * P1; // 800
+
+    // cv::Ptr<cv::StereoBM> bm = cv::StereoBM::create(16,9);
+    cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(0, nDisp, SADWin, P1, P2,
+                                                          1, preFC, 10, 100, 32,
+                                                          cv::StereoSGBM::MODE_SGBM);
+
     // bool ok;
     // auto &&request = api->SelectStreamRequest(&ok); // ask user to select a stream
-    const mynteye::StreamRequest request = mynteye::StreamRequest(1280, 400, mynteye::Format::YUYV, 30);
+    const StreamRequest request = StreamRequest(2 * width, height, Format::YUYV, 30);
 
     // if (!ok) return 1;
     api->ConfigStreamRequest(request);
@@ -48,13 +65,13 @@ int main(int argc, char *argv[])
 
     api->EnableStreamData(Stream::LEFT_RECTIFIED);
     api->EnableStreamData(Stream::RIGHT_RECTIFIED);
-    api->SetDisparityComputingMethodType(DisparityComputingMethod::SGBM);
-    api->EnableStreamData(Stream::DISPARITY_NORMALIZED);
+    // api->SetDisparityComputingMethodType(DisparityComputingMethod::SGBM);
+    // api->EnableStreamData(Stream::DISPARITY_NORMALIZED);
     api->Start(Source::VIDEO_STREAMING);
 
-    
-    api->SetOptionValue(Option::EXPOSURE_MODE, 0); // auto exposure
-        // max_gain: range [0,255], default 8
+    // auto exposure
+    api->SetOptionValue(Option::EXPOSURE_MODE, 0);
+    // max_gain: range [0,255], default 8
     api->SetOptionValue(Option::MAX_GAIN, 8);
     // max_exposure_time: range [0,655], default 333
     api->SetOptionValue(Option::MAX_EXPOSURE_TIME, 333);
@@ -68,33 +85,40 @@ int main(int argc, char *argv[])
 
     cv::namedWindow("frame");
 
+    // cv::namedWindow("disparity_normalized");
+
+    printf("\n");
     while (true)
     {
         api->WaitForStreams();
 
         auto &&left_data = api->GetStreamData(Stream::LEFT_RECTIFIED);
         auto &&right_data = api->GetStreamData(Stream::RIGHT_RECTIFIED);
-        auto &&disp_norm_data = api->GetStreamData(Stream::DISPARITY_NORMALIZED);
-
-        cv::Mat img;
+        // auto &&disp_norm_data = api->GetStreamData(Stream::DISPARITY_NORMALIZED);
+        // printf("num of channels is %d\n", left_data.frame.channels());
+        cv::Mat img, disp, disp8;
         if (!left_data.frame.empty() && !right_data.frame.empty())
-        {
-            // double t_c = cv::getTickCount() / cv::getTickFrequency();
-            // fps = 1.0 / (t_c - t);
-            // printf("\r%02.2f", fps);
-            // t = t_c;
-            cv::hconcat(left_data.frame, right_data.frame, img);
-            cv::imshow("frame", img);
-        }
-
-        if (!disp_norm_data.frame.empty())
         {
             double t_c = cv::getTickCount() / cv::getTickFrequency();
             fps = 1.0 / (t_c - t);
-            printf("%02.2f\n", fps);
+            printf("\r%02.2f", fps);
             t = t_c;
-            cv::imshow("disparity_normalized", disp_norm_data.frame); // CV_8UC1
+            cv::hconcat(left_data.frame, right_data.frame, img);
+            cv::imshow("frame", img);
+
+            sgbm->compute(left_data.frame, right_data.frame, disp);
+            disp.convertTo(disp8, CV_8U, 255/(nDisp*16.));
+            cv::imshow("disp", disp8);
         }
+
+        // if (!disp_norm_data.frame.empty())
+        // {
+        //     double t_c = cv::getTickCount() / cv::getTickFrequency();
+        //     fps = 1.0 / (t_c - t);
+        //     printf("%02.2f\n", fps);
+        //     t = t_c;
+        //     cv::imshow("disparity_normalized", disp_norm_data.frame); // CV_8UC1
+        // }
 
         char key = static_cast<char>(cv::waitKey(1));
         if (key == 27 || key == 'q' || key == 'Q')
